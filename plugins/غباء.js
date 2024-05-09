393b7515e8fa4977be8e6d28e6064ed469c7f369
ragibnabil <166906006+ragibnabil@users.noreply.github.com>
2024-05-09 12:12:31 +0300

    Update رجوله.js

--- plugins/غباء.js
+++ plugins/غباء.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, command, text }) => {
-let stupidity = `*نسبه القنط *
-*نسبة القنط ${text} هي* *${Math.floor(Math.random() * 100)}%* *من 100%*
+let stupidity = `*نسبه رجولتك *
+*نسبة رجولتك ${text} هي* *${Math.floor(Math.random() * 100)}%* *من 100%*
 *ربنا يهديكم💙☁️*
 `.trim()
 m.reply(stupidity, null, { mentions: conn.parseMention(stupidity) })}
 handler.help = ['stupidity']
 handler.tags = ['']
-handler.command = /^(قنط)$/i
+handler.command = /^(رجولتي)$/i
 export default handler
