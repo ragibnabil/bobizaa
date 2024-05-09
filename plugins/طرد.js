132e24ac84fe16b28f0025754df82e2c171b68ea
ragibnabil <166906006+ragibnabil@users.noreply.github.com>
2024-05-09 11:14:21 +0300

    Update طرد.js

--- plugins/طرد.js
+++ plugins/طرد.js
@@ -1,12 +1,12 @@
 let handler = async (m, { conn, participants, usedPrefix, command }) => {
 
-let kickte = `*منشن ع الشخص الي هتطرده بره يا حب🥺❤*`
+let kickte = `*تاقي العاوز تطردو*`
 
 if (!m.mentionedJid[0] && !m.quoted) return m.reply(kickte, m.chat, { mentions: conn.parseMention(kickte)}) 
 let user = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted.sender
 let owr = m.chat.split`-`[0]
 await conn.groupParticipantsUpdate(m.chat, [user], 'remove')
-m.reply(`*GOOOOOOOOOOOOOOOOOOOOD BYE Hhhhhh*`) 
+m.reply(`*المدفعية عطبرة بوووووڤ🔥🚫*`) 
 
 }
 
