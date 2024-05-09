e3c0327e85866dc617530ddac664faa2ab796c06
ragibnabil <166906006+ragibnabil@users.noreply.github.com>
2024-05-09 10:23:51 +0300

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -124,7 +124,7 @@ contextInfo: {
 externalAdReply: {
 title: 'DOCTOR BOT ♥',
 body: "أول بوت واتساب ديني 💖",
-thumbnailUrl: 'https://telegra.ph/file/2829c7653514416d207e2.jpg',
+thumbnailUrl: 'https://picjj.com/image/mrousavy2326020283013642303.pzVan',
 sourceUrl: 'https://instagram.com/00o5k',
 mediaType: 1,
 renderLargerThumbnail: true
