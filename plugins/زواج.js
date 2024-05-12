384540bcf3b564e2bbf95189d17338f09568deeb
ragibnabil <166906006+ragibnabil@users.noreply.github.com>
2024-05-12 18:08:18 +0300

    Update and rename حجره ورقه.js to زواج.js

---
+++ plugins/زواج.js
@@ -0,0 +1,16 @@
+let toM = a => '@' + a.split('@')[0]
+function handler(m, { groupMetadata }) {
+let ps = groupMetadata.participants.map(v => v.id)
+let a = ps.getRandom()
+let b
+do b = ps.getRandom()
+while (b === a)
+m.reply(`*${toM(a)}, الف مبروك للعريس 🥳*
+*${toM(b)}, الف مبروك للعروسه ♥*\n\*متنسوش تعزمونا بق علي الفرح 😂*`, null, {
+mentions: [a, b]
+})}
+handler.help = ['زواج']
+handler.tags = ['']
+handler.command = ['formarpareja','زواج']
+handler.group = true
+export default handler
