76b3600125e4055aec2311e843d1803d41e2c596
ragibnabil <166906006+ragibnabil@users.noreply.github.com>
2024-05-25 10:21:26 +0300

    Update main.js

--- main.js
+++ main.js
@@ -490,7 +490,7 @@ setInterval(async () => {
   if (stopped === 'close' || !conn || !conn.user) return;
   const _uptime = process.uptime() * 1000;
   const uptime = clockString(_uptime);
-  const bio = `DOCTOR BOT [ ⏳ ] Uptime: ${uptime}`;
+  const bio = `اذكر الله [ ⏳ ] Uptime: ${uptime}`;
   await conn.updateProfileStatus(bio).catch((_) => _);
 }, 60000);
 function clockString(ms) {
